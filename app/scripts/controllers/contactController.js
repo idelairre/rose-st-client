a9d79f4a3021ec6fb999a5d1b4f3c8b128d85dc4
idelairre <idelairre@gmail.com>
2015-06-18 18:49:50 -0400

    attempting to fix contact form

--- app/scripts/controllers/contactController.js
+++ app/scripts/controllers/contactController.js
@@ -2,10 +2,10 @@
 
 angular.module('roseStClient').controller('ContactController', function ($http, $scope, ServerUrl) {
 	
-	var contact = {};
+	 var contact = {};
     
     $scope.submitForm = function(data) {
         console.log("posting data....");
-        $http.post('localhost:9000/mail/contact_me.php', JSON.stringify(data)).success(function(){/*success callback*/});
+        $http.post('././mail/contact_me.php', JSON.stringify(data)).success(function(){/*success callback*/});
     };
 });
