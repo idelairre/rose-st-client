ea8ba59f7f5189154302c293a78dbf549fdd1431
idelairre <idelairre@gmail.com>
2015-06-18 19:01:53 -0400

    another attempt at getting the php form to work.

--- app/scripts/controllers/contactController.js
+++ app/scripts/controllers/contactController.js
@@ -1,11 +1,26 @@
 'use strict';
 
 angular.module('roseStClient').controller('ContactController', function ($http, $scope, ServerUrl) {
-	
-	 var contact = {};
-    
-    $scope.submitForm = function(data) {
-        console.log("posting data....");
-        $http.post('././mail/contact_me.php', JSON.stringify(data)).success(function(){/*success callback*/});
-    };
+
+	$scope.contact = {};
+
+	//    $scope.submitForm = function(data) {
+	//        console.log("posting data....");
+	//        $http.post('././mail/contact_me.php', JSON.stringify(data)).success(function(response){
+	//			  console.log(response)
+	//		  }).error(function(response) {
+	//			  console.log(response);
+	//		  })
+	//	 }
+
+	$scope.submitForm = function (data) {
+		$http({
+			method: 'POST',
+			url: 'process.php',
+			data: $.param(data), // pass in data as strings
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			} // set the headers so angular passing info as form data (not request payload)
+		})
+	}
 });
