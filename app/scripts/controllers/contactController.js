c29d68fb539a42b826adee3787ab2cae6b37e154
idelairre <idelairre@gmail.com>
2015-06-18 19:39:24 -0400

    github pages does not support php, evidently. Restores original form, clears out contact form of bum code.

--- app/scripts/controllers/contactController.js
+++ app/scripts/controllers/contactController.js
@@ -1,27 +1,6 @@
 'use strict';
 
 angular.module('roseStClient').controller('ContactController', function ($http, $scope, ServerUrl) {
-
-//	$scope.contact = {};
-	//    $scope.submitForm = function(data) {
-	//        console.log("posting data....");
-	//        $http.post('././mail/contact_me.php', JSON.stringify(data)).success(function(response){
-	//			  console.log(response)
-	//		  }).error(function(response) {
-	//			  console.log(response);
-	//		  })
-	//	 }
-
-//	$scope.submitForm = function (data) {
-//		$http({
-//			method: 'POST',
-//			url: '././mail/contact_me.php',
-//			data: $.param(data), // pass in data as strings
-//			headers: {
-//				'Content-Type': 'application/x-www-form-urlencoded'
-//			} // set the headers so angular passing info as form data (not request payload)
-//		})
-//	}
 	
 	 $scope.result = 'hidden'
     $scope.resultMessage;
