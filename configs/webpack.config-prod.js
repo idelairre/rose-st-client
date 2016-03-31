e712d9c95bb1ecf1732673242079635c26a59612
Ian <idelairre@gmail.com>
2016-03-31 02:55:12 -0400

    fixes dependency with webpack production config

--- configs/webpack.config-prod.js
+++ configs/webpack.config-prod.js
@@ -1,6 +1,10 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
+var InlineEnviromentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
 
-config.plugins.unshift(new webpack.EnvironmentPlugin(['STRIPE']), new InlineEnviromentVariablesPlugin({ HOSTNAME: 'https://rose-st-api.herokuapp.com' }));
+config.plugins.unshift(
+  new webpack.EnvironmentPlugin(['STRIPE']),
+  new InlineEnviromentVariablesPlugin({ HOSTNAME: 'https://rose-st-api.herokuapp.com' })
+);
 
 module.exports = config;
